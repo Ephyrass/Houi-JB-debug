2db6833c720926b9ecb48a92cb17b259dae63ca9
Ephyras <jb.houi@yahoo.fr>
2025-03-04 15:54:24 +0100

    remove unnecessary comments

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -7,21 +7,23 @@ import java.io.FileWriter;
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	
 	private static int rashCount = 0;		
-	private static int pupilCount = 0;		
+	private static int pupilCount = 0;	
 	
 	public static void main(String args[]) throws Exception {
+		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-	
+		int i = 0;	
+		int headCount = 0;	
 		while (line != null) {
-
+			i++;	
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
+				headCount++;
+				System.out.println("number of headaches: " + headCount);
 			}
-			else if (line.equals("rash")) {
+			else if (line.equals("rush")) {
 				rashCount++;
 			}
 			else if (line.contains("pupils")) {
